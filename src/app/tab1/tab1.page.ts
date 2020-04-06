21dfb7c1ca84e7ae1ce94f96936ee27ea3cf78ae
DeAndre Boston <dboston3@yahoo.com>
2020-04-05 20:14:16 -0500

    Initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Activity } from '../types';
+import { ActivityService } from '../activity.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  activityList: Observable<Activity[]>;
 
-  constructor() {}
-
+  constructor(activityService: ActivityService) {
+    this.activityList = activityService.getAllActivities();
+  }
 }
